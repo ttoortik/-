ed1410d25fa0e048630e27697de5f48bafb3b426
ttoortik <55398612+ttoortik@users.noreply.github.com>
2019-09-17 12:20:41 +0500

    Update 42.py
    .

--- 42.py
+++ 42.py
@@ -6,6 +6,6 @@ for word in words:
     letters = 0
     for letter in word:
         letters +=alphabet.index(letter)
-        if letters == int(math.sqrt(2*letters)) * (int(math.sqrt(2*letters))+1)/2:
-            result+=1
+    if letters == int(math.sqrt(2*letters)) * (int(math.sqrt(2*letters))+1)/2:
+        result+=1
 print(result)
